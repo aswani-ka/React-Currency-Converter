00f717fe86b9182a23fcd29dac1a4aaa16beb883
ASWANI K A <aswanika05@gmail.com>
2025-11-14 13:52:10 +0530

    chore: add basic layout using Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App() {
 
   
   return (
-    <>
-      
-    </>
+    <div className='flex flex-wrap h-screen w-full justify-center items-center bg-cover bg-no-repeat' style={{backgroundImage: 'url(https://images.pexels.com/photos/6266445/pexels-photo-6266445.jpeg)'}}>
+      <h1 className='text-[3rem] text-white font-bold'>Currency Converter</h1>
+    </div>
   )
 }
 
