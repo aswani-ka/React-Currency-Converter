91b0744ce09b21a1c8434e8651bbf5f80951a188
ASWANI K A <aswanika05@gmail.com>
2025-11-14 14:54:41 +0530

    feat: create CurrencyInput component and import to index.js

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useId } from 'react'
 
-function InputBox() {
+function InputBox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions=[],
+    selectedCurrency="usd",
+    amountDisabled,
+    currencyDisabled,
+    className=""
+}) {
+
+    const id = useId()
     
   return (
-    <div>InputBox</div>
+    <div>
+        <div>
+            <label htmlFor={id}>{label}</label>
+            <input 
+            id={id} 
+            type="number" 
+            placeholder='Amount'
+            value={amount}
+            onChange={(e) => {
+                onAmountChange && onAmountChange(Number(e.target.value))
+            }}
+            disabled={amountDisabled}
+            />
+        </div>
+        <div>
+            Curreny Type
+            <select 
+            className=''
+            value={selectedCurrency}
+            onChange={(e) => {
+                onCurrencyChange && onCurrencyChange(e.target.value)
+            }}
+            disabled={currencyDisabled}
+            >
+                {currencyOptions.map((currency) => (
+                    <option key={currency} value={currency}>{currency}</option>
+                ))}
+            </select>
+        </div>
+    </div>
   )
 }
 
